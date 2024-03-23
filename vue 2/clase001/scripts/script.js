eeed39466162393544c497a66e6d17b404fe0bbe
Stoicos <marcelo.anavia.web@gmail.com>
2024-03-23 11:12:54 -0300

    delete vue 2 folder

--- vue 2/clase001/scripts/script.js
+++
@@ -1,9 +0,0 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-      message: 'Hola Vue!',
-      titulo: 'No voy a cambiar',
-      verDiv: false,
-      html: '<p>Voy a cambiar el html</p>',
-    }
-})
