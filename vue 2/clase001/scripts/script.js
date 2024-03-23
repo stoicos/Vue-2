bd2ba5fdbc1037006f1f3e59753242151e413f0d
stoicos <136374793+stoicos@users.noreply.github.com>
2024-03-23 10:27:06 -0300

    Add files via upload

---
+++ vue 2/clase001/scripts/script.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hola Vue!',
+      titulo: 'No voy a cambiar',
+      verDiv: false,
+      html: '<p>Voy a cambiar el html</p>',
+    }
+})
